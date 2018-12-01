a913304f2854d025ff5cedca1ad62af279eee973
Unknown <ocebeki@gmail.com>
2018-12-01 11:46:08 +0000

    slider

---
+++ script/index.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    $('.footer__iconBox').hover(inn, outt);
+
+    function inn() {
+        $(this).find('path').css('fill', '#000');
+
+    }
+
+    function outt() {
+        $(this).find('path').css('fill', '#fff')
+    }
+
+
+});
+
+$(document).ready(function () {
+    $('.slick').slick({
+        // autoplay: true,
+        autoplaySpeed: 2000,
+        fade: true,
+        arrows: true,
+
+    });
+});
