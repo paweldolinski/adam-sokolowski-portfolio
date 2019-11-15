6e86050eca2c679427b121a7e0d2013bea040a44
Unknown <ocebeki@gmail.com>
2019-11-15 13:41:18 +0100

    portfolio image

--- contactForm.php
+++ contactForm.php
@@ -11,28 +11,33 @@
 
 <body>
     <nav class="nav">
-        <div class="nav__logo">
-            <a href="index.html">
-                <img src="img/logobiale.svg">
-            </a>
+        <div class="nav__wrapper">
+            <div class="nav__logo">
+                <a href='index.html'>
+                    <h1>ADAM SOKOLOWSKI</h1>
+                </a>
+            </div>
+            <div class="nav__toggleBtn">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <ul>
+                <li><a href="index.html">HOME</a></li>
+                <li><a href="about.html">ABOUT ME</a></li>
+                <li><a href="portfolio.html">PORTFOLIO</a></li>
+                <li><a href="contact.html">CONTACT</a></li>
+            </ul>
         </div>
-        <ul class="nav__list">
-            <li><a href="index.html">HOME</a></li>
-            <li><a href="portfolio.html">WORK</a></li>
-            <li><a href="contact.html">CONTACT</a>
-        </ul>
     </nav>
     <div class="contactForm">
         <h1>Message has been sent!!</h1>
-        <h1><a href="index.html">Back to the website.</a></h1>
+        <button><a href="index.html">Back to the website.</a></button>
     </div>
 
 </body>
 
 </html>
-
-
-
 
 
 <?php 
@@ -41,84 +46,14 @@
     $email = $_POST['email'];
     $message = $_POST['message'];
 
-    $email_from = 'nec87@wp.pl';
+    $email_from = '';
     
     $email_body = "User Name: $name,$lastName.\n".
                     "User Email: $email.\n".
                         "User Message: $message.\n";
 
-    $to = "ocebeki@gmail.com";
+    $to = "asfotarto@gmail.com";
 
     mail($to, $email, $email_body);
     header("Location: contact.html");
-
-
 ?>
-
-<?php
-
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-        // access
-        $secretKey = '6Lc2H3wUAAAAACSKawCnvRjqo8MC3nLmfzFOUTAJ';
-        $captcha = $_POST['g-recaptcha-response'];
-
-        if(!$captcha){
-          echo '<p class="alert alert-warning">Please check the the captcha form.</p>';
-          exit;
-        }
-
-        # FIX: Replace this email with recipient email
-        $mail_to = "ocebeki@gmail.com";
-        
-        # Sender Data
-        $subject = trim($_POST["subject"]);
-        $name = str_replace(array("\r","\n"),array(" "," ") , strip_tags(trim($_POST["name"])));
-        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-        $phone = trim($_POST["phone"]);
-        $message = trim($_POST["message"]);
-        
-        if ( empty($name) OR !filter_var($email, FILTER_VALIDATE_EMAIL) OR empty($phone) OR empty($subject) OR empty($message)) {
-            # Set a 400 (bad request) response code and exit.
-            http_response_code(400);
-            echo '<p class="alert alert-warning">Please complete the form and try again.</p>';
-            exit;
-        }
-
-        $ip = $_SERVER['REMOTE_ADDR'];
-        $response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$secretKey."&response=".$captcha."&remoteip=".$ip);
-        $responseKeys = json_decode($response,true);
-
-        if(intval($responseKeys["success"]) !== 1) {
-          echo '<p class="alert alert-warning">Please check the the captcha form.</p>';
-        } else {
-            # Mail Content
-            $content = "Name: $name\n";
-            $content .= "Email: $email\n\n";
-            $content .= "Phone: $phone\n";
-            $content .= "Message:\n$message\n";
-
-            # email headers.
-            $headers = "From: $name <$email>";
-
-            # Send the email.
-            $success = mail($mail_to, $subject, $content, $headers);
-            if ($success) {
-                # Set a 200 (okay) response code.
-                http_response_code(200);
-                echo '<p class="alert alert-success">Thank You! Your message has been sent.</p>';
-            } else {
-                # Set a 500 (internal server error) response code.
-                http_response_code(500);
-                echo '<p class="alert alert-warning">Oops! Something went wrong, we couldnt send your message.</p>';
-            }
-        }
-
-    } else {
-        # Not a POST request, set a 403 (forbidden) response code.
-        http_response_code(403);
-        echo '<p class="alert alert-warning">There was a problem with your submission, please try again.</p>';
-    }
-
-?>
-
--- script/index.js
+++ script/index.js
@@ -9,15 +9,22 @@ $(document).ready(function () {
     function outt() {
         $(this).find('path').css('fill', '#fff')
     }
-    
-        $('.nav__toggleBtn').click(function(){
+
+    $('.nav__toggleBtn').click(function () {
         $('.nav__wrapper').toggleClass('nav__wrapper--toggle');
         $('.nav__toggleBtn span').toggleClass('active')
-            
-        })
-    
 
-
+    })
 });
 
-
+const contact = document.querySelector('.contact')
+if (contact) {
+    grecaptcha.ready(function () {
+        grecaptcha.execute('6LeAuYwUAAAAAIC8y9Muv1v0Z-psHBPmXfRWXi7v', {
+            action: 'action_name'
+        })
+            .then(function (token) {
+                // Verify the token on the server.
+            });
+    });
+} 
